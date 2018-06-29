dcb6480fc85f23a99da41de59a907f702379af8d
Thomas Tortorini <thomastortorini@gmail.com>
2018-06-29 07:49:00 +0200

    beautify the code

--- canvas-pixelsfinger.js
+++ canvas-pixelsfinger.js
@@ -1,140 +1,123 @@
-/*
-	canvas-pixelsfinger - 1.1.0
-	https://github.com/Mr21/canvas-pixelsfinger
-*/
+function pixelsFinger(
+	ctx,
+	radius,
+	srcX, srcY,
+	dstX, dstY,
+	intensity
+) {
+	radius = Math.round( radius );
+	srcX = Math.round( srcX );
+	srcY = Math.round( srcY );
+	dstX = Math.round( dstX );
+	dstY = Math.round( dstY );
 
-( function() {
+	let xImgPart,
+		yImgPart,
+		wImgPart,
+		hImgPart;
 
-	window.pixelsFinger = function(
-		ctx,
-		radius,
-		srcX, srcY,
-		dstX, dstY,
-		intensity
-	) {
+	xImgPart = Math.min( srcX, dstX ), xImgPart -= Math.min( radius, xImgPart );
+	yImgPart = Math.min( srcY, dstY ), yImgPart -= Math.min( radius, yImgPart );
+	wImgPart = Math.max( srcX, dstX ), wImgPart += Math.min( radius, ctx.canvas.width - wImgPart );
+	hImgPart = Math.max( srcY, dstY ), hImgPart += Math.min( radius, ctx.canvas.height - hImgPart );
 
-		radius = Math.round( radius );
-		srcX = Math.round( srcX );
-		srcY = Math.round( srcY );
-		dstX = Math.round( dstX );
-		dstY = Math.round( dstY );
+	if ( wImgPart <= 1 || hImgPart <= 1 ) {
+		return;
+	}
 
-		var
-			w = ctx.canvas.width,
-			h = ctx.canvas.height,
-			xImgPart, yImgPart,
-			wImgPart, hImgPart
-		;
+	let exec = false,
+		execImage = 0,
+		x = srcX - xImgPart,
+		y = srcY - yImgPart,
+		xSrcInt = x,
+		ySrcInt = y,
+		xDstInt = x,
+		yDstInt = y,
+		xinc,
+		yinc;
 
-		xImgPart = Math.min( srcX, dstX ), xImgPart -= Math.min( radius, xImgPart );
-		yImgPart = Math.min( srcY, dstY ), yImgPart -= Math.min( radius, yImgPart );
-		wImgPart = Math.max( srcX, dstX ), wImgPart += Math.min( radius, w - wImgPart );
-		hImgPart = Math.max( srcY, dstY ), hImgPart += Math.min( radius, h - hImgPart );
+	const images = [
+			ctx.getImageData( xImgPart, yImgPart, wImgPart, hImgPart ),
+			ctx.getImageData( xImgPart, yImgPart, wImgPart, hImgPart )
+		],
+		px = [
+			images[ 0 ].data,
+			images[ 1 ].data
+		],
+		squareRadius = radius * radius,
+		vx = dstX - srcX,
+		vy = dstY - srcY,
+		avx = Math.abs( vx ),
+		avy = Math.abs( vy );
 
-		if ( wImgPart <= 1 || hImgPart <= 1 ) {
-			return;
+	if ( avx > avy ) {
+		xinc = vx > 0 ? 1 : -1;
+		yinc = vx !== 0 ? vy / avx : 0;
+	} else {
+		xinc = vy !== 0 ? vx / avy : 0;
+		yinc = vy > 0 ? 1 : -1;
+	}
+
+	for ( let ite = Math.max( avx, avy ); ite >= 0; --ite ) {
+		x += xinc;
+		y += yinc;
+		if ( xSrcInt !== Math.round( x ) ) {
+			exec = true;
+			xSrcInt = xDstInt;
+			xDstInt = Math.round( x );
 		}
-
-		var
-			exec = false,
-			execImage = 0,
-			images = [
-				ctx.getImageData( xImgPart, yImgPart, wImgPart, hImgPart ),
-				ctx.getImageData( xImgPart, yImgPart, wImgPart, hImgPart )
-			],
-			px = [
-				images[ 0 ].data,
-				images[ 1 ].data
-			],
-			x = srcX - xImgPart,
-			y = srcY - yImgPart,
-			xinc,
-			yinc,
-			xSrcInt = x,
-			ySrcInt = y,
-			xDstInt = x,
-			yDstInt = y,
-			squareRadius = radius * radius,
-			vx = dstX - srcX,
-			vy = dstY - srcY,
-			avx = Math.abs( vx ),
-			avy = Math.abs( vy ),
-			ite = Math.max( avx, avy )
-		;
-
-		if ( avx > avy ) {
-			xinc = vx > 0 ? 1 : -1;
-			yinc = vx !== 0 ? vy / avx : 0;
-		} else {
-			xinc = vy !== 0 ? vx / avy : 0;
-			yinc = vy > 0 ? 1 : -1;
+		if ( ySrcInt !== Math.round( y ) ) {
+			exec = true;
+			ySrcInt = yDstInt;
+			yDstInt = Math.round( y );
 		}
-
-		while ( ite-- > 0 ) {
-			x += xinc;
-			y += yinc;
-			if ( xSrcInt !== Math.round( x ) ) {
-				exec = true;
-				xSrcInt = xDstInt;
-				xDstInt = Math.round( x );
-			}
-			if ( ySrcInt !== Math.round( y ) ) {
-				exec = true;
-				ySrcInt = yDstInt;
-				yDstInt = Math.round( y );
-			}
-			if ( exec ) {
-				exec = false;
-				proc(
-					px[ execImage ],
-					px[ execImage = +!execImage ],
-					xSrcInt, ySrcInt,
-					xDstInt, yDstInt
-				);
-			}
+		if ( exec ) {
+			exec = false;
+			proc(
+				px[ execImage ],
+				px[ execImage = +!execImage ],
+				xSrcInt, ySrcInt,
+				xDstInt, yDstInt
+			);
 		}
+	}
 
-		ctx.putImageData( images[ execImage ], xImgPart, yImgPart );
+	ctx.putImageData( images[ execImage ], xImgPart, yImgPart );
 
-		function ind( x, y ) {
-			return ( x + y * wImgPart ) * 4;
-		}
+	function ind( x, y ) {
+		return ( x + y * wImgPart ) * 4;
+	}
 
-		function proc( pxsrc, pxdst, x, y, dx, dy ) {
-			var ix, iy, dist, si, di;
+	function proc( pxsrc, pxdst, x, y, dx, dy ) {
+		for ( let iy = -radius; iy < radius; ++iy ) {
+			for ( let ix = -radius; ix < radius; ++ix ) {
+				const dist = ix * ix + iy * iy;
 
-			for (     iy = -radius; iy < radius; ++iy ) {
-				for ( ix = -radius; ix < radius; ++ix ) {
+				if ( dist < squareRadius &&
+					dx + ix >= 0 && dx + ix < wImgPart &&
+					dy + iy >= 0 && dy + iy < hImgPart
+				) {
+					const di = ind( dx + ix, dy + iy );
 
-					dist = ix * ix + iy * iy;
 					if (
-						dist < squareRadius &&
-						dx + ix >= 0 && dx + ix < wImgPart &&
-						dy + iy >= 0 && dy + iy < hImgPart
+						x + ix < 0 || x + ix >= wImgPart ||
+						y + iy < 0 || y + iy >= hImgPart
 					) {
+						pxdst[ di     ] =
+						pxdst[ di + 1 ] =
+						pxdst[ di + 2 ] =
+						pxdst[ di + 3 ] = 0;
+					} else {
+						const si = ind( x + ix, y + iy ),
+							dist2 = ( 1 - dist / squareRadius ) * intensity;
 
-						di = ind( dx + ix, dy + iy );
-
-						if (
-							x + ix < 0 || x + ix >= wImgPart ||
-							y + iy < 0 || y + iy >= hImgPart
-						) {
-							pxdst[ di     ] =
-							pxdst[ di + 1 ] =
-							pxdst[ di + 2 ] =
-							pxdst[ di + 3 ] = 0;
-						} else {
-							si = ind( x + ix, y + iy );
-							dist = ( 1 - dist / squareRadius ) * intensity;
-							pxdst[ di     ] += ( pxsrc[ si    ] - pxdst[ di     ] ) * dist;
-							pxdst[ di + 1 ] += ( pxsrc[ si + 1] - pxdst[ di + 1 ] ) * dist;
-							pxdst[ di + 2 ] += ( pxsrc[ si + 2] - pxdst[ di + 2 ] ) * dist;
-							pxdst[ di + 3 ] += ( pxsrc[ si + 3] - pxdst[ di + 3 ] ) * dist;
-						}
+						pxdst[ di     ] += ( pxsrc[ si     ] - pxdst[ di     ] ) * dist2;
+						pxdst[ di + 1 ] += ( pxsrc[ si + 1 ] - pxdst[ di + 1 ] ) * dist2;
+						pxdst[ di + 2 ] += ( pxsrc[ si + 2 ] - pxdst[ di + 2 ] ) * dist2;
+						pxdst[ di + 3 ] += ( pxsrc[ si + 3 ] - pxdst[ di + 3 ] ) * dist2;
 					}
 				}
 			}
 		}
-	};
-
-})();
+	}
+}
